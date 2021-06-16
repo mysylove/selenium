20e2571a774454b5532467ffb8068c2a4b1a4666
jaejungko <jaejungko@gmail.com>
2021-06-16 18:05:49 +0900

    ㅡ먀ㅜ

--- selenium003.py
+++ selenium003.py
@@ -6,6 +6,14 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
+def FindnClick_span_by_xpath(driver, xpath, text):
+    for elem in driver.find_elements_by_xpath(xpath):
+        if elem.text != "":
+            print(elem.text)
+        if elem.text == text:
+            elem.click()
+            break
+
 delay = 0.1
 path = "D:\SRC\GitHub\selenium\chromedriver.exe"
 options = webdriver.ChromeOptions()
@@ -26,31 +34,19 @@ elem2 = browser.find_element_by_id("loginForm2")
 elem2.clear()
 elem2.send_keys("wowjddl!@34")
 
-print("find elemLogin-Start")
 time.sleep(1)
-
-for elemLogin in browser.find_elements_by_xpath('.//span[@class="ng-scope"]'):
-    print(elemLogin.text)
-    if elemLogin.text == "로그인":
-        elemLogin.click()
-        break
-
-browser.implicitly_wait(1)
-
-print("find elemIssueMgmt-Start")
-time.sleep(1)
-
-for elemIssueMgmt in browser.find_elements_by_xpath('.//span[@class="ng-scope"]'):
-    print(elemIssueMgmt.text)
-    if elemIssueMgmt.text == "이슈 관리":
-        elemIssueMgmt.click()
-        break
-
-#issue_url = 'http://www.owlsolution.co.kr:8080/#/issues/issueList/'
-#browser.get(issue_url)
-
-#browser.find_element_by_xpath("//span[contains(@class, 'ng-scope')]").click()
-
-#browser.find_element_by_class_name("ng-scope").click()
+print("find 로그인-Start")
+FindnClick_span_by_xpath(browser, './/span[@class="ng-scope"]', '로그인')
+#browser.implicitly_wait(2)
 
 time.sleep(1)
+print("find 이슈 관리-Start")
+FindnClick_span_by_xpath(browser, './/span[@class="ng-scope"]', '이슈 관리')
+#browser.implicitly_wait(2)
+
+time.sleep(1)
+print("find 상세검색-Start")
+FindnClick_span_by_xpath(browser, './/span[@class="ng-scope"]', '상세검색')
+#browser.implicitly_wait(2)
+
+time.sleep(3)
